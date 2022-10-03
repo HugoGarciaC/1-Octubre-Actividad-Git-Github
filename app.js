d1fc0f3120453e29a22fa2d4666c14373387527c
Hugo Alberto García Clavijo <hagarciac@siesa.com>
2022-10-03 17:00:30 -0500

    Ahora la funcion suma cuatro numeros

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-function suma(a, b){
-    sumaNum = a + b;
+function suma(a, b, c, d){
+    sumaNum = a + b + c + d;
 return sumaNum;
 }
 
 //Call
-console.log(suma(5, 8));
+console.log(suma(5, 8, 5, 2));
